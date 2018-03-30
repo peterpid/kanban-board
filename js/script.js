1f9ead37b6cbbe6f75f1413922ae6e2803d7716a
Piotr Pidlypczak <peterpid@gmail.com>
2018-03-30 13:17:06 +0200

    Add styles using Bootstrap, add title for Card
    - add styles for Card using Media object

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(function() {
 
+    function isEmpty(str) {
+        return (!str || 0 === str.length);
+    }
+
     function randomString() {
         var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
         var str = '';
@@ -9,6 +13,7 @@ $(function() {
         return str;
     }
 
+    //COLUMN
     function Column(name) {
         var self = this; // useful for nested functions
 
@@ -17,9 +22,9 @@ $(function() {
         this.$element = createColumn();
 
         function createColumn() {
-            var $column         = $('<div>').addClass('column');
+            var $column         = $('<div>').addClass('col');
             var $columnTitle    = $('<h2>').addClass('column-title').text(self.name);
-            var $columnCardList = $('<ul>').addClass('column-card-list');
+            var $columnCardList = $('<ul>').addClass('column-card-list list-unstyled');
             var $columnDelete   = $('<button>').addClass('btn-delete').text('x');
             var $columnAddCard  = $('<button>').addClass('add-card').text('Add a card');
             
@@ -29,7 +34,10 @@ $(function() {
             
             //Add a note after clicking on the button:
             $columnAddCard.click(function() {
-                self.addCard(new Card(prompt("Enter the name of the card")));
+                var cardName = prompt('Enter the name of the card', 'New task 1');
+                if (!isEmpty(cardName)){
+                    self.addCard(new Card(cardName));
+                }
             });        
 
             $column.append($columnTitle)
@@ -52,26 +60,33 @@ $(function() {
     };
 
     //CARD
-    function Card(description) {
+    function Card(title, description) {
         var self = this;
 
         this.id = randomString();
+        this.title = title || 'Task name';
         this.description = description;
         this.$element = createCard();
 
         function createCard() {
             var $card = $('<li>').addClass('card');
+            var $cardContatiner = $('<div>').addClass('media');
+            var $cardBody = $('<div>').addClass('card-body media-body');
+            var $cardTitle = $('<h5>').addClass('card-title mt-0').text(self.title);
             var $cardDescription = $('<p>').addClass('card-description').text(self.description);
-            var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+            var $cardDelete = $('<button>').addClass('btn-delete align-self-center ml-3').text('x');
 
             $cardDelete.click(function(){
                 self.removeCard();
             });
 
-            $card.append($cardDelete)
+            $card.append($cardContatiner);
+            $cardContatiner.append($cardBody)
+                .append($cardDelete);
+            $cardBody.append($cardTitle)
                 .append($cardDescription);
+                
             return $card;     
-
         }
     }
 
@@ -98,10 +113,35 @@ $(function() {
     };
 
     $('.create-column').click(function(){
-        var name = prompt('Enter a column name');
-        var column = new Column(name);
-            board.addColumn(column);
+        var name = prompt('Enter a column name', 'New column');
+        if (!isEmpty(name)) {
+            var column = new Column(name);
+                board.addColumn(column);
+        }
     });
 
+    // CREATING COLUMNS
+    var todoColumn = new Column('To do');
+    var inprogressColumn = new Column('In progress');
+    var doneColumn = new Column('Done');
+
+    // ADDING COLUMNS TO THE BOARD
+    board.addColumn(todoColumn);
+    board.addColumn(inprogressColumn);
+    board.addColumn(doneColumn);
+
+    // CREATING CARDS
+    var card1 = new Card('Mission to Mars', 'Invent new rocket engine');
+    var card2 = new Card('Task2');
+    var card3 = new Card('Task3');
+    var card4 = new Card('Task4');
+    var card5 = new Card('Task5');
+
+    // ADDING CARDS TO COLUMNS
+    todoColumn.addCard(card1);
+    todoColumn.addCard(card2);
+    inprogressColumn.addCard(card3);
+    inprogressColumn.addCard(card4);
+    doneColumn.addCard(card5);
 
 });
